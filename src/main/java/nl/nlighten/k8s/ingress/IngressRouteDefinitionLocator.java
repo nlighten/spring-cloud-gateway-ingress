4b12a8aa982466d958d7a9864e82ebcd7bc9b234
richard.swart@nlighten.nl <richard.swart@nlighten.nl>
2019-06-03 22:08:28 +0200

    reformat

--- src/main/java/nl/nlighten/k8s/ingress/IngressRouteDefinitionLocator.java
+++ src/main/java/nl/nlighten/k8s/ingress/IngressRouteDefinitionLocator.java
@@ -28,85 +28,81 @@ import java.util.concurrent.ConcurrentMap;
 
 @Component
 public class IngressRouteDefinitionLocator
-		implements RouteDefinitionLocator, Watcher<Ingress> {
-	private static final Logger log = LoggerFactory
-			.getLogger(IngressRouteDefinitionLocator.class);
-	private static final String INGRESS_CLASS = "spring.cloud.gateway";
-	private final ConcurrentMap<String, RouteDefinition> routeDefinitions = new ConcurrentHashMap<>();
-	private final ApplicationEventPublisher eventPublisher;
-	private final KubernetesClient kubernetesClient;
-	private final ObjectMapper objectMapper;
+        implements RouteDefinitionLocator, Watcher<Ingress> {
+    private static final Logger log = LoggerFactory
+            .getLogger(IngressRouteDefinitionLocator.class);
+    private static final String INGRESS_CLASS = "spring.cloud.gateway";
+    private final ConcurrentMap<String, RouteDefinition> routeDefinitions = new ConcurrentHashMap<>();
+    private final ApplicationEventPublisher eventPublisher;
+    private final KubernetesClient kubernetesClient;
+    private final ObjectMapper objectMapper;
 
-	public IngressRouteDefinitionLocator(ApplicationEventPublisher eventPublisher,
-                                                   KubernetesClient kubernetesClient) {
-		this.eventPublisher = eventPublisher;
-		this.kubernetesClient = kubernetesClient;
-		this.objectMapper = new Jackson2ObjectMapperBuilder().factory(new YAMLFactory())
-				.build();
-	}
+    public IngressRouteDefinitionLocator(ApplicationEventPublisher eventPublisher,
+                                         KubernetesClient kubernetesClient) {
+        this.eventPublisher = eventPublisher;
+        this.kubernetesClient = kubernetesClient;
+        this.objectMapper = new Jackson2ObjectMapperBuilder().factory(new YAMLFactory())
+                .build();
+    }
 
-	@Override
-	public Flux<RouteDefinition> getRouteDefinitions() {
-		return Flux.fromIterable(this.routeDefinitions.values());
-	}
+    @Override
+    public Flux<RouteDefinition> getRouteDefinitions() {
+        return Flux.fromIterable(this.routeDefinitions.values());
+    }
 
-	@PostConstruct
-	public void watch() {
-		this.kubernetesClient.extensions().ingresses().inAnyNamespace().watch(this);
-	}
+    @PostConstruct
+    public void watch() {
+        this.kubernetesClient.extensions().ingresses().inAnyNamespace().watch(this);
+    }
 
-	@Override
-	public void eventReceived(Action action, Ingress ingress) {
-		try {
-			ObjectMeta metadata = ingress.getMetadata();
-			String id = metadata.getNamespace() + "/" + metadata.getName();
-			if (action == Action.ADDED || action == Action.MODIFIED) {
-				Map<String, String> annotations = metadata.getAnnotations();
-				if ((annotations != null) && annotations.containsKey("kubernetes.io/ingress.class") && annotations.get("kubernetes.io/ingress.class").equals(INGRESS_CLASS)) {
-					String routes = annotations.get(INGRESS_CLASS + "/routes");
-					if (routes == null) {
-						log.warn("No {}/routes annotation found on ingress definition {}/{}. Ignoring this ingress definition.", INGRESS_CLASS, metadata.getNamespace(), metadata.getName());
-						return;
-					}
-					// ingress must have a default backend
-					if (ingress.getSpec().getBackend() == null) {
-						log.warn("No default backend found on ingress definition {}/{}. Ignoring this ingress definition.", metadata.getNamespace(), metadata.getName());
-						return;
-					}
-					String serviceName = ingress.getSpec().getBackend().getServiceName();
-		//			int port = ingress.getSpec().getBackend().getServicePort().getIntVal();
-  				    RouteDefinition routeDefinition = this.objectMapper.readValue(routes, RouteDefinition.class);
-					routeDefinition.setId(id);
-					if (routeDefinition.getUri() == null) {
-							URI uri = UriComponentsBuilder.newInstance() //
-									.scheme("lb") //
-									.host(serviceName) //
-		//							.port(port) //
-									.build().toUri();
-							routeDefinition.setUri(uri);
-						}
-						String yaml = this.objectMapper
-								.writeValueAsString(routeDefinition);
-						log.info("Create or update ingress: {}\t{}", id, yaml);
-						this.routeDefinitions.put(routeDefinition.getId(),
-								routeDefinition);
-						this.eventPublisher.publishEvent(new RefreshRoutesEvent(this));
-								}
-			}
-			else if (action == Action.DELETED) {
-				log.info("Delete {}", id);
-				this.routeDefinitions.remove(id);
-				this.eventPublisher.publishEvent(new RefreshRoutesEvent(this));
-			}
-		}
-		catch (IOException e) {
-			throw new UncheckedIOException(e);
-		}
-	}
+    @Override
+    public void eventReceived(Action action, Ingress ingress) {
+        try {
+            ObjectMeta metadata = ingress.getMetadata();
+            String id = metadata.getNamespace() + "/" + metadata.getName();
+            if (action == Action.ADDED || action == Action.MODIFIED) {
+                Map<String, String> annotations = metadata.getAnnotations();
+                if ((annotations != null) && annotations.containsKey("kubernetes.io/ingress.class") && annotations.get("kubernetes.io/ingress.class").equals(INGRESS_CLASS)) {
+                    String routes = annotations.get(INGRESS_CLASS + "/routes");
+                    if (routes == null) {
+                        log.warn("No {}/routes annotation found on ingress definition {}/{}. Ignoring this ingress definition.", INGRESS_CLASS, metadata.getNamespace(), metadata.getName());
+                        return;
+                    }
+                    // ingress must have a default backend
+                    if (ingress.getSpec().getBackend() == null) {
+                        log.warn("No default backend found on ingress definition {}/{}. Ignoring this ingress definition.", metadata.getNamespace(), metadata.getName());
+                        return;
+                    }
+                    String serviceName = ingress.getSpec().getBackend().getServiceName();
+                    RouteDefinition routeDefinition = this.objectMapper.readValue(routes, RouteDefinition.class);
+                    routeDefinition.setId(id);
+                    if (routeDefinition.getUri() == null) {
+                        URI uri = UriComponentsBuilder.newInstance()
+                                .scheme("lb")
+                                .host(serviceName)
+                                .build().toUri();
+                        routeDefinition.setUri(uri);
+                    }
+                    String yaml = this.objectMapper
+                            .writeValueAsString(routeDefinition);
+                    log.info("Create or update ingress: {}\t{}", id, yaml);
+                    this.routeDefinitions.put(routeDefinition.getId(),
+                            routeDefinition);
+                    this.eventPublisher.publishEvent(new RefreshRoutesEvent(this));
+                }
+            } else if (action == Action.DELETED) {
+                log.info("Delete {}", id);
+                this.routeDefinitions.remove(id);
+                this.eventPublisher.publishEvent(new RefreshRoutesEvent(this));
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
 
 
-	@Override
-	public void onClose(KubernetesClientException e) {
-		log.debug("close");
-	}
+    @Override
+    public void onClose(KubernetesClientException e) {
+        log.debug("close");
+    }
 }
